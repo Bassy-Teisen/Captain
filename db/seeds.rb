06dbfcae04106c6ed9db4e8bf343439ce3153544
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-11-10 10:32:50 +1000

    Initail commit

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+profile = [
+    {name: "Captian Cook", phone_number: "23425565", captain: true, description: "dhfgf", email: "23q45t"},
+    {name: "Jack Sparrow", phone_number: "230985565", captain: true, description: "dhfgf", email: "23q45t"},
+    {name: "Jonny Bravo", phone_number: "2342745665", captain: false, description: "dhfgf", email: "23q45t"},
+    {name: "Goku", phone_number: "2342556789", captain: false, description: "dhfgf", email: "23q45t"}
+]
+
+# Profile.create()
+
+Profile.destroy_all
+Profile.create!(profile)
